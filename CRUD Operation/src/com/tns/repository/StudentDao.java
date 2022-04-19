171f6bbd0c1a975833698cca3c1143fe46ddd968
gaurav <gaurav@DESKTOP-UO5KS4V>
2022-04-19 22:02:11 +0530

    CRUD Operations

---
+++ CRUD Operation/src/com/tns/repository/StudentDao.java
@@ -0,0 +1,22 @@
+package com.tns.repository;
+
+import com.tns.entities.Student; 
+ 
+public interface StudentDao { 
+ 
+ 	public abstract Student getStudentById(int id); 
+ 
+ 	public abstract void addStudent(Student student); 
+ 
+ 	public abstract void removeStudent(Student student); 
+ 
+ 	public abstract void updateStudent(Student student); 
+ 	 
+ 	 
+ 
+ 	public abstract void commitTransaction(); 
+ 
+ 	public abstract void beginTransaction(); 
+ 
+} 
+
